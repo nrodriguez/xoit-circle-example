01c8704d01ca1a7c9d725e5e66ee54fde0314992
Nephtali Rodriguez <nrodriguez@xogrp.com>
2017-02-13 15:21:47 -0500

    Adds another test to explode my own ego

--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Widget, type: :model do
-  it "does something" do
+  it "ensures that JustNeph is pretty awesome" do
+    expect(true).to be_truthy
+  end
+
+  it "verifies that he is indeed not modest either" do
     expect(true).to be_truthy
   end
 end
